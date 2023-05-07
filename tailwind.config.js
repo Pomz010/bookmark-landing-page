e6a42a979babe01dcc906ed9b3f9e220b5e3b0b7
Pomz010 <rollcastillo010@gmail.com>
2023-05-07 23:27:55 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  purge: ["./dist/**/*.html"],
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Rubik: ["rubik", "Geneva", "Verdana", "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
+
